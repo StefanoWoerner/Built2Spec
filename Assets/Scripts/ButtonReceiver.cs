b4b8222a7d718e4db73ba7f8a0740008cf8309ee
Stefano Woerner <mail@swoerner.eu>
2018-12-08 14:11:42 +0100

    Basic UI and saving/loading of .obj files

---
+++ Assets/Scripts/ButtonReceiver.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.Receivers;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using System.IO;
+
+#if WINDOWS_UWP
+
+using System;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Threading;
+using System.Threading.Tasks;
+
+#endif
+
+public class ButtonReceiver : InteractionReceiver
+{
+    public GameObject textObjectState;
+    private TextMesh txt;
+    //public FileSurfaceObserver fso;
+    public string meshFileName;
+    public GameObject mapObject;
+
+    void Start()
+    {
+        txt = textObjectState.GetComponentInChildren<TextMesh>();
+    }
+
+    protected override void InputDown(GameObject obj, InputEventData eventData)
+    {
+        Debug.Log(obj.name + " : InputDown");
+        txt.text = obj.name + " : InputDown";
+
+        switch (obj.name)
+        {
+            case "ToolbarButton1":
+                Debug.Log("Saving...");
+                //MeshSaver.Save(meshFileName, SpatialMappingManager.Instance.GetMeshFilters());
+                ObjSaver.Save(meshFileName, SpatialMappingManager.Instance.GetMeshFilters());
+                txt.text = "Mesh saved to file 1";
+                break;
+
+            case "ToolbarButton2":
+                Debug.Log("Loading...");
+                Debug.Log(Path.Combine(ObjSaver.MeshFolderName, meshFileName + ".obj"));
+                txt.text = "1: " + Path.Combine(ObjSaver.MeshFolderName, meshFileName + ".obj");
+                Material material2 = Resources.Load("defaultMat", typeof(Material)) as Material;
+                OBJLoader.LoadOBJFile(Path.Combine(ObjSaver.MeshFolderName, meshFileName + ".obj"), material2, mapObject);
+                txt.text = "2: " + Path.Combine(ObjSaver.MeshFolderName, meshFileName + ".obj");
+                //int larifari = 0;
+                //foreach (Transform child in go2.transform)
+                //{
+                //    MeshRenderer meshr = child.gameObject.GetComponent<MeshRenderer>();
+                //    meshr.material = material2;
+                //    larifari++;
+                //}
+                //txt.text = "Number: " + larifari;
+                break;
+
+            case "ToolbarButton4":
+                Debug.Log("Loading...");
+#if !UNITY_EDITOR && UNITY_WSA
+                //Task<Task> task = Task<Task>.Factory.StartNew(
+                //        async () =>
+                //        {
+                //            FileOpenPicker openPicker = new FileOpenPicker();
+                //            openPicker.ViewMode = PickerViewMode.Thumbnail;
+                //            openPicker.SuggestedStartLocation = PickerLocationId.Objects3D;
+                //            openPicker.FileTypeFilter.Add(".obj");
+                //            StorageFile file = await openPicker.PickSingleFileAsync();
+                //            //StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(folderName);
+                //            //StorageFile file = await folder.GetFileAsync(fileName);
+                //            IRandomAccessStreamWithContentType randomAccessStream = await file.OpenReadAsync();
+                //            stream = randomAccessStream.AsStreamForRead();
+                //        });
+                //task.Wait();
+                //task.Result.Wait();
+                //GameObject go3 = OBJLoader.LoadOBJFile(OpenFileAsync());
+                OpenFileAsync();
+#else
+                Debug.Log("Hello");
+#endif
+                break;
+
+            default:
+                break;
+        }
+    }
+
+#if !UNITY_EDITOR && UNITY_WSA
+    private async void OpenFileAsync()
+    {
+        UnityEngine.WSA.Application.InvokeOnUIThread(async () =>
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.Objects3D;
+            openPicker.FileTypeFilter.Add(".obj");
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+            {
+                if (file != null)
+                {
+                    // Application now has read/write access to the picked file
+                    Debug.Log("Picked file: " + file.DisplayName + ", Path: " + file.Path);
+                    txt.text = "Picked file: " + file.DisplayName + ", Path: " + file.Path;
+                }
+                else
+                {
+                    // The picker was dismissed with no selected file
+                    Debug.Log("File picker operation cancelled");
+                    txt.text = "File picker operation cancelled";
+                }
+
+
+            }, true);
+
+
+        }, false);
+
+    }
+#endif
+
+}
---
+++ Assets/Scripts/ObjSaver.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+#if !UNITY_EDITOR && UNITY_WSA
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+#endif
+
+public static class ObjSaver
+{
+    /// <summary>
+    /// The extension given to mesh files.
+    /// </summary>
+    private static string fileExtension = ".obj";
+
+    /// <summary>
+    /// Read-only property which returns the folder path where mesh files are stored.
+    /// </summary>
+    public static string MeshFolderName
+    {
+        get
+        {
+#if !UNITY_EDITOR && UNITY_WSA
+                return ApplicationData.Current.RoamingFolder.Path;
+#else
+            return Application.persistentDataPath;
+#endif
+        }
+    }
+
+    public static int vertexCount;
+    public static string Save(string fileName, IEnumerable<Mesh> meshes)
+    {
+        if (string.IsNullOrEmpty(fileName))
+        {
+            throw new ArgumentException("Must specify a valid fileName.");
+        }
+
+        if (meshes == null)
+        {
+            throw new ArgumentNullException("Value of meshes cannot be null.");
+        }
+
+        // Create the mesh file.
+        String folderName = MeshFolderName;
+        Debug.Log(String.Format("Saving mesh file: {0}", Path.Combine(folderName, fileName + fileExtension)));
+
+        vertexCount = 0;
+        using (StreamWriter outputFile = new StreamWriter(OpenFileForWrite(folderName, fileName + fileExtension)))
+        {
+
+            int o = 0;
+            foreach (Mesh theMesh in meshes)
+            {
+                o++;
+                outputFile.WriteLine("o Object." + o);
+                outputFile.Write(MeshToString(theMesh, vertexCount));
+                outputFile.WriteLine("");
+            }
+        }
+
+        Debug.Log("Mesh file saved.");
+
+        return Path.Combine(folderName, fileName + fileExtension);
+    }
+
+    public static string Save(string fileName, IEnumerable<MeshFilter> meshFilters)
+    {
+        if (string.IsNullOrEmpty(fileName))
+        {
+            throw new ArgumentException("Must specify a valid fileName.");
+        }
+
+        if (meshFilters == null)
+        {
+            throw new ArgumentNullException("Value of meshFilters cannot be null.");
+        }
+
+        // Create the mesh file.
+        String folderName = MeshFolderName;
+        Debug.Log(String.Format("Saving mesh file: {0}", Path.Combine(folderName, fileName + fileExtension)));
+
+        vertexCount = 0;
+        using (StreamWriter outputFile = new StreamWriter(OpenFileForWrite(folderName, fileName + fileExtension)))
+        {
+
+            int o = 0;
+            foreach (MeshFilter meshFilter in meshFilters)
+            {
+                o++;
+                outputFile.WriteLine("o Object." + o);
+                outputFile.Write(MeshToString(meshFilter.sharedMesh, vertexCount, meshFilter.transform));
+                outputFile.WriteLine("");
+            }
+        }
+
+        Debug.Log("Mesh file saved.");
+
+        return Path.Combine(folderName, fileName + fileExtension);
+    }
+
+    public static string MeshToString(Mesh m, int lastVertexIndex = 0, Transform transform = null)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        vertexCount += m.vertices.Length;
+        if (transform != null)
+        {
+            foreach (Vector3 v in m.vertices)
+            {
+                Vector3 vt = transform.TransformPoint(v);
+                sb.Append(string.Format("v {0} {1} {2}\n", -vt.x, vt.y, vt.z));
+            }
+        }
+        else
+        {
+            foreach (Vector3 v in m.vertices)
+            {
+                sb.Append(string.Format("v {0} {1} {2}\n", -v.x, v.y, v.z));
+            }
+        }
+        
+        //sb.Append("\n");
+        //foreach (Vector3 v in m.normals)
+        //{
+        //    sb.Append(string.Format("vn {0} {1} {2}\n", v.x, v.y, v.z));
+        //}
+        //sb.Append("\n");
+        //foreach (Vector3 v in m.uv)
+        //{
+        //    sb.Append(string.Format("vt {0} {1}\n", v.x, v.y));
+        //}
+        for (int material = 0; material < m.subMeshCount; material++)
+        {
+
+            int[] triangles = m.GetTriangles(material);
+            for (int i = 0; i < triangles.Length; i += 3)
+            {
+                //faceCount += 3;
+                sb.Append(string.Format("f {2} {1} {0}\n",
+                    triangles[i] + 1 + lastVertexIndex, triangles[i + 1] + 1 + lastVertexIndex, triangles[i + 2] + 1 + lastVertexIndex));
+            }
+        }
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Opens the specified file for reading.
+    /// </summary>
+    /// <param name="folderName">The name of the folder containing the file.</param>
+    /// <param name="fileName">The name of the file, including extension. </param>
+    /// <returns>Stream used for reading the file's data.</returns>
+    private static Stream OpenFileForRead(string folderName, string fileName)
+    {
+        Stream stream = null;
+
+#if !UNITY_EDITOR && UNITY_WSA
+            Task<Task> task = Task<Task>.Factory.StartNew(
+                            async () =>
+                            {
+                                StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(folderName);
+                                StorageFile file = await folder.GetFileAsync(fileName);
+                                IRandomAccessStreamWithContentType randomAccessStream = await file.OpenReadAsync();
+                                stream = randomAccessStream.AsStreamForRead();
+                            });
+            task.Wait();
+            task.Result.Wait();
+#else
+        stream = new FileStream(Path.Combine(folderName, fileName), FileMode.Open, FileAccess.Read);
+#endif
+        return stream;
+    }
+
+    /// <summary>
+    /// Opens the specified file for writing.
+    /// </summary>
+    /// <param name="folderName">The name of the folder containing the file.</param>
+    /// <param name="fileName">The name of the file, including extension.</param>
+    /// <returns>Stream used for writing the file's data.</returns>
+    /// <remarks>If the specified file already exists, it will be overwritten.</remarks>
+    private static Stream OpenFileForWrite(string folderName, string fileName)
+    {
+        Stream stream = null;
+
+#if !UNITY_EDITOR && UNITY_WSA
+            Task<Task> task = Task<Task>.Factory.StartNew(
+                            async () =>
+                            {
+                                StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(folderName);
+                                StorageFile file = await folder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+                                IRandomAccessStream randomAccessStream = await file.OpenAsync(FileAccessMode.ReadWrite);
+                                stream = randomAccessStream.AsStreamForWrite();
+                            });
+            task.Wait();
+            task.Result.Wait();
+#else
+        stream = new FileStream(Path.Combine(folderName, fileName), FileMode.Create, FileAccess.Write);
+#endif
+        return stream;
+    }
+}
+
